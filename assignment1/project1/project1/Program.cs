49bb4c9abca40fa1ea24772bf70dcfecb1151d81
MXH <812556783@qq.com>
2023-03-05 15:40:10 +0800

    assignment1

---
+++ assignment1/project1/project1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace project1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            double n1, n2, result;
+            string t, op;
+            Console.WriteLine("Please input the 1st number:");
+            t = Console.ReadLine();
+            n1 = Double.Parse(t);
+            Console.WriteLine("Please input the 2nd number:");
+            t = Console.ReadLine();
+            n2 = Double.Parse(t);
+            Console.WriteLine("Please select an operation type:");
+            op = Console.ReadLine();
+            switch (op)
+            {
+                case "+":
+                    result = n1 + n2;
+                    Console.WriteLine("The result is:" + result);
+                    break;
+                case "-":
+                    result = n1 - n2;
+                    Console.WriteLine("The result is:" + result);
+                    break;
+                case "*":
+                    result = n1 * n2;
+                    Console.WriteLine("The result is:" + result);
+                    break;
+                case "/":
+                    if (n2 == 0)
+                        Console.WriteLine("Divide by zero error!");
+                    else
+                    {
+                        result = n1 / n2;
+                        Console.WriteLine("The result is:" + result);
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Misinput!");
+                    break;
+            }
+        }
+    }
+}
