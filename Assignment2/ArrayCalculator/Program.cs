53e013f0da0ab4915b67d85f67781b342057041e
ZK-0002 <812556783@qq.com>
2023-03-07 00:23:21 +0800

    part of assignment2
    part of assignment2

---
+++ Assignment2/ArrayCalculator/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace ArrayCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = GetArray("Please choose:\n1.Default array\n2.Custom array\n");
+            int maxValue = GetMax(myArray);
+            Console.WriteLine("Max value: " + maxValue);
+            int minValue = GetMin(myArray);
+            Console.WriteLine("Min value: " + minValue);
+            int sum = GetSum(myArray);
+            Console.WriteLine("Sum of array elements: " + sum);
+            double avg = GetAverage(myArray);
+            Console.WriteLine("Average of array elements: " + avg);
+        }
+
+        static int[] GetArray(string s)
+        {
+            int op;
+            int[] array = {32, 15, 7, 9, 21, 8, 17, 4, 81, 55};
+            do
+            {
+                Console.Write(s);
+            } while (!int.TryParse(Console.ReadLine(), out op) || (op != 1 && op !=2));;
+            if (op == 1)
+            {
+                Console.WriteLine("The default array is: 32, 15, 7, 9, 21, 8, 17, 4, 81, 55");
+            }
+            if (op == 2)
+            {
+                Console.WriteLine("Please enter some integers (e.g. 1,2,3): ");
+                string input = Console.ReadLine();
+                string[] inputArray = input.Split(',');
+                int[] intArray = new int[inputArray.Length];
+                for (int i = 0; i < inputArray.Length; i++)
+                {
+                    intArray[i] = int.Parse(inputArray[i]);
+                }
+                Array.Resize(ref array, 0);
+                array = intArray;
+            }
+            return array;
+        }
+
+        static int GetMax(int[] arr)
+        {
+            int max = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+            return max;
+        }
+
+        static int GetMin(int[] arr)
+        {
+            int min = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+            return min;
+        }
+
+        static int GetSum(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum;
+        }
+
+        static double GetAverage(int[] arr)
+        {
+            double avg = 0;
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            if (arr.Length > 0)
+            {
+                avg = (double)sum / arr.Length;
+            }
+            return avg;
+        }
+    }
+}
---
+++ Assignment2/PrimeFactor/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace PrimeFactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = GetUserInput("Please enter an integer greater than 1: ");
+            int[] primeFactors = FindPrimeFactors(number);
+            primeFactors = Merge(primeFactors);
+            Console.WriteLine($"The prime factor(s) of {number} is/are: {string.Join(", ", primeFactors)}");
+        }
+
+        static int GetUserInput(string s)
+        {
+            int input;
+            do
+            {
+                Console.Write(s);
+            } while (!int.TryParse(Console.ReadLine(), out input) || input <= 1);
+            return input;
+        }
+
+        static int[] FindPrimeFactors(int number)
+        {
+            int[] factors = new int[0];
+            for (int i = 2; i <= number; i++)
+            {
+                if (number % i == 0 && IsPrime(i))
+                {
+                    Array.Resize(ref factors, factors.Length + 1);
+                    factors[factors.Length - 1] = i;
+                    number /= i;
+                    i--;
+                }
+            }
+            return factors;
+        }
+
+        static bool IsPrime(int number)
+        {
+            if (number < 2) return false;
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0) return false;
+            }
+            return true;
+        }
+
+        static int[] Merge(int[] arr)
+        {
+            int[] factors = new int[1];
+            factors[0] = arr[0];
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] != arr[i + 1])
+                {
+                    Array.Resize(ref factors, factors.Length + 1);
+                    factors[factors.Length - 1] = arr[i + 1];
+                }
+            }
+            return factors;
+        }
+    }
+}
