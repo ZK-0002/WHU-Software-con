68ae563bbc9c78c4e643d1439d17c932232bbfb1
ZK-0002 <812556783@qq.com>
2023-03-07 22:36:40 +0800

    assignment2

--- Assignment2/SieveOfEratosthenes/Program.cs
+++ Assignment2/SieveOfEratosthenes/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SieveOfEratosthenes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> primes = FindPrimes(100);
+            Console.WriteLine($"Prime numbers from 2 to 100 are: {string.Join(", ", primes)}");
+        }
+
+        static List<int> FindPrimes(int upperLimit)
+        {
+            List<int> primes = new List<int>();
+            bool[] isComposite = new bool[upperLimit];
+            for (int i = 2; i <= upperLimit; i++)
+            {
+                if (!isComposite[i - 1])
+                {
+                    primes.Add(i);
+                    for (int j = 2; i * j <= upperLimit; j++)
+                    {
+                        isComposite[i * j - 1] = true;
+                    }
+                }
+            }
+            return primes;
+        }
+    }
+}
--- Assignment2/ToeplitzMatrix/Program.cs
+++ Assignment2/ToeplitzMatrix/Program.cs
@@ -1,2 +1,75 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ToeplitzMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = ReadMatrix();
+            bool upperTriangle = UpperTriangle(matrix);
+            bool lowerTriangle = LowerTriangle(matrix);
+            bool isToeplitz = upperTriangle & lowerTriangle;
+            Console.WriteLine(isToeplitz ? "True" : "False");
+        }
+        
+        static int[,] ReadMatrix()
+        {
+            Console.WriteLine("Please enter a matrix:");
+            Console.WriteLine("e.g.\n2 3\n1 2 3\n1 1 2\n");
+            string[] firstLine = Console.ReadLine().Split();
+            int rows = int.Parse(firstLine[0]);
+            int cols = int.Parse(firstLine[1]);
+            int[,] matrix = new int[rows, cols];
+            for (int i = 0; i < rows; i++)
+            {
+                string[] rowLine = Console.ReadLine().Split();
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = int.Parse(rowLine[j]);
+                }
+            }
+            return matrix;
+        }
+
+        static bool UpperTriangle(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            for (int i = 0; i < cols; i++)
+            {
+                int val = matrix[0, i];
+                int row = 1;
+                int col = i + 1;
+                while (row < rows && col < cols)
+                {
+                    if (matrix[row, col] != val)
+                        return false;
+                    row++;
+                    col++;
+                }
+            }
+            return true;
+        }
+
+        static bool LowerTriangle(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            for (int i = 1; i < rows; i++)
+            {
+                int val = matrix[i, 0];
+                int row = i + 1;
+                int col = 1;
+                while (row < rows && col < cols)
+                {
+                    if (matrix[row, col] != val)
+                        return false;
+                    row++;
+                    col++;
+                }
+            }
+            return true;
+        }
+    }
+}
